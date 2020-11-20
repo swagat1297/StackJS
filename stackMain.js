34fe405c7df67e36270d13022739fcfe257c0594
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-20 18:52:09 +0530

    Added: UserChoice for implementing stack

--- stackMain.js
+++ stackMain.js
@@ -1,3 +1,22 @@
 let utility = require("./Util.js");
-utility.push();
-utility.popElement();
+let readlineSync = require('readline-sync');
+
+userChoice = function () {
+    let isTerminated = true;
+    while (isTerminated == true) {
+        let choice = readlineSync.question("\nEner your choice:\n1.push\n2.pop\n3.ShowStack\n4.exit\n");
+        switch (choice.trim()) {
+            case '1': let newValue = readlineSync.question("Enter push Integer:");
+                utility.push(newValue);
+                break;
+            case '2': utility.popElement();
+                break;
+            case '3': utility.showStack();
+                break;    
+            case '4': isTerminated = false;
+                break;
+            default: console.log("Invalid input");
+        }
+    }
+}
+userChoice();
